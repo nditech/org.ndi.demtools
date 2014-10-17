d90599f7fffff92b8e620ff31d36bfc5fd9e90f8
Karl Fallstrom <karl@nthdegreeit.com>
2014-10-17 17:18:20 +0000

    final translation changes

--- translate.php
+++
@@ -1,7 +0,0 @@
-<?php
-civicrm_initialize();
-$config=CRM_Core_Config::singleton( ); 
-print_r($config->lcMessages);
-print ts('New Contacts by Month',array('domain' => 'org.ndi.ndicivimp'));
-print "\n";
-CRM_Core_Error::backtrace();
